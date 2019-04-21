8c36a3253f56ad264b117dde8289aed389611669
jigsaw <jigsaw@jigsaw.localdomain>
2019-04-21 13:48:33 +0200

    STARTED DIGIT RECOGNITION

--- third_tutorial.py
+++ third_tutorial.py
@@ -1,19 +1,10 @@
 from neural_network import NeuralNetwork
 import numpy as np
-import matplotlib as plot
+import matplotlib.pyplot as plot
+from sklearn.datasets import load_digits
 
-if __name__ == "__main()__":
-	main()
+digits = load_digits()
 
-
-def main():
-	nn = NeuralNetwork()
-	
-	W = {}
-	b = {}
-	for l in range(1, len(nn_structure)):
-		W[l] = np.random.random_sample((nn_structure[l], nn_structure[l-1]))
-		b[l] = r.random_sample((nn_structure[l],))
-	
-	
-	nn.setup_weights(weights);
+plot.gray()
+plot.matshow(digits.images[1])
+plot.show()
