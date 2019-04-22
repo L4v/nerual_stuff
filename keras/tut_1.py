e1c6bcf88e8112c6fe59e12c6a7d59b2ce4a06a1
jigsaw <jigsaw@jigsaw.localdomain>
2019-04-22 11:50:28 +0200

    Created small NM in Keras

--- keras/tut_1.py
+++ keras/tut_1.py
@@ -1,7 +1,7 @@
 #https://youtu.be/UkzhouEk6uY
 import numpy as np
 from random import randint
-from sklern.preprocessing import MinMaxScalar
+from sklearn.preprocessing import MinMaxScaler
 
 train_labels = []
 train_samples = []
@@ -11,35 +11,39 @@ train_samples = []
 # 95% 65+ had side effects
 # 95% <65 had no side effects
 
-for i in range(1000):
-	random_younger = randint(13, 64)
-	train_samples.append(random_younger)
-	train_labels.append(0)
-	
-	random_older = randint(65, 100)
-	train_samples.append(random_older)
-	train_labels.append(1)
-	
 for i in range(50):
+	# 5% of the younger population which experienced the side effects
 	random_younger = randint(13, 64)
 	train_samples.append(random_younger)
 	train_labels.append(1)
 	
+	# 5% of the older population which hadn't experienced the side effects
 	random_older = randint(65, 100)
 	train_samples.append(random_older)
 	train_labels.append(0)
+	
+for i in range(1000):
+	# 95% of the younger population which hadn't experienced the side effects
+	random_younger = randint(13, 64)
+	train_samples.append(random_younger)
+	train_labels.append(0)
+	
+	# 95% of the older population which experienced the side effects
+	random_older = randint(65, 100)
+	train_samples.append(random_older)
+	train_labels.append(1)
 	
 
 # Print the raw data
 print("Train samples:")
 for i in train_samples:
-	print(i + " "),
+	print(i, " "),
 	
 train_labels = np.array(train_labels)
 train_samples = np.array(train_samples)
 
 # Scaling the samples
-scaler = MinMaxScaler(feature_range(0,1)) # Scales from 0 to 1
+scaler = MinMaxScaler(feature_range=(0,1)) # Scales from 0 to 1
 scaled_train_samples = scaler.fit_transform((train_samples).reshape(-1, 1)) # Because this fit transform function doesn't accept 1D arrays
 																			# it's reshaped from -1 to 1
 
--- keras/tut_2.py
+++ keras/tut_2.py
@@ -1,5 +1,8 @@
-#https://youtu.be/Boo6SmgmHuM
+#https://youtu.be/Boo6SmgmHuM DONE
+#https://www.youtube.com/watch?v=dzoh8cfnvnI DONE
+#https://www.youtube.com/watch?v=2f-NjDUvZIE
 import keras
+from tut_1 import scaled_train_samples, train_labels
 from keras import Sequential
 from keras.layers import Activation
 from keras.layers.core import Dense
@@ -14,3 +17,11 @@ model = Sequential([
 #model.add(l4)
 
 model.summary() # Prints the model visualization
+
+model.compile(Adam(lr=.001), loss = 'sparse_categorical_crossentropy', metrics=['accuracy']) # Adam - optimization function(gradient descent, etc..), lr - learning rate
+
+																							 # loss - loss function (mean square error, etc...), metrics - on what to judge performance of model
+model.fit(scaled_train_samples, train_labels, validation_split=0.1, # Training, samples to train on, labels for the samples, validation_data will be 10% of the training samples
+		 batch_size=10, epochs=20, shuffle=True, verbose=2) # batch size, epoch - how many runs
+
+
